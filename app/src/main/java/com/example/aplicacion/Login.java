526f893d849bd39d9ac899b8fee06b8c355ac50c
marcos <marcosromlu@gmail.com>
2023-11-15 13:54:04 +0100

    animation and splash added

--- app/src/main/java/com/example/aplicacion/Login.java
+++ app/src/main/java/com/example/aplicacion/Login.java
@@ -3,8 +3,13 @@ package com.example.aplicacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 /**
  * @author marcos
@@ -16,6 +21,17 @@ import android.view.View;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageView mGirl=findViewById(R.id.girl);
+
+        Glide.with(this)
+                //.load(aqui se introduce la url)
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))  //muestra la imagen lentamente
+                .centerCrop()
+                //.circleCrop()
+                //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .into(mGirl);
     }
 
     public void openMain(View v){
@@ -29,4 +45,6 @@ import android.view.View;
         Intent intent=new Intent(Login.this, Signup.class);
         startActivity(intent);
     }
+    
+
 }
---
+++ app/src/main/java/com/example/aplicacion/Splash.java
@@ -0,0 +1,59 @@
+package com.example.aplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        openApp();
+
+
+
+    ImageView thunder = findViewById(R.id.logosplash);
+    Animation myanim = AnimationUtils.loadAnimation(this, R.anim.blink);
+    thunder.startAnimation(myanim);
+
+
+
+
+    ImageView fondo = findViewById(R.id.fondo);
+
+       Glide.with(this)
+                .load("https://www.tuexperto.com/wp-content/uploads/2023/01/50-fondos-de-pantalla-para-movil-de-paisajes-y-tematica-alegre-11.jpg")
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+               //.diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+            .into(fondo);
+
+    }
+
+
+    private void openApp(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash
+                        .this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        },5000);
+    }
+}
